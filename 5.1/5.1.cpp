6ba4ed625377890a80763287e391becaac0aa19e
Ott Nikita <joleanymty799@gmail.com>
2024-05-11 03:53:22 +0300

    5. БИБЛИОТЕКА ШАБЛОНОВ STL

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,49 @@
+﻿/*  Условие задания из первого семестра: 
+Преобразовать строку: после каждой буквы 'z' добавить символ '!'. 
+Получить в алфавитном порядке все согласные латинские буквы, входящие в строку
+*/
+
+#include <iostream>
+#include <string>
+#include <locale> 
+#include <cctype> 
+#include <algorithm> // для функции сортировки
+using namespace std;
+
+
+string transformString(const string& input_string) {  // преобразование строки
+    string result;
+    for (char c : input_string) {
+        result += c;
+        if (c == 'z') {
+            result += '!';
+        }
+    }
+    return result;
+}
+
+string findsoglasnieletters(const string& input_string) {  // поиск согласных букв
+    string soglasnie;
+    for (char c : input_string) {
+        if (isalpha(c) && tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' && tolower(c) != 'o' && tolower(c) != 'u') {
+            soglasnie += tolower(c);
+        }
+    }
+    sort(soglasnie.begin(), soglasnie.end()); // сортировка согласных букв в алфавитном порядке
+    return soglasnie;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    string stroka;
+    cout << "Введите строку: ";
+    cin >> stroka;
+
+    string stroka2 = transformString(stroka); // преобразование строки
+    string soglasnie = findsoglasnieletters(stroka2); // поиск согласных букв
+
+    cout << "Преобразованная строка: " << stroka2 << endl;
+    cout << "Согласные буквы в алфавитном порядке: " << soglasnie << endl;
+    return 0;
+}
+
---
+++ 5.2/5.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> // для функции reverse
+using namespace std;
+
+
+string translatenumber(int numb) {
+    const string hexadecimal = "0123456789ABCDEF";
+    string result;
+
+    while (numb > 0) {  // перевод из десятичной в шестнадцатеричную систему счисления
+        int remainder = numb % 16;
+        result += hexadecimal[remainder];
+        numb /= 16;
+    }
+
+    reverse(result.begin(), result.end()); // результат возвращается в обратном порядке, поэтому переворачиваем его
+    return result;
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    string str;
+    cout << "Введите число в десятичной системе: " << endl;
+    cin >> str;
+
+    // проверка правильности ввода числа
+    if (str.find_first_not_of("0123456789-") != string::npos || (str.find('-') != string::npos && str.find('-') != 0)) {
+        cout << "Неверный ввод числа." << endl;
+        return 0;
+    }
+
+    int numb = stoi(str); // преобразует в тип int
+
+    // если число отрицательное, меняем знак и продолжаем работу с абсолютным значением
+    bool isNegative = false;
+    if (numb < 0) {
+        isNegative = true;
+        numb *= -1;
+    }
+
+    string result = translatenumber(numb);
+
+    // если число было отрицательным, добавляем знак "-"
+    if (isNegative)
+        result = "-" + result;
+
+    cout << "В 16-ой системе: " << result << endl;
+    return 0;
+}
